3a040d1b3abe6c7e574f1fa626c0a8e46cab088f
Andres Gonzalez Camacho <gonzalezcamachoandres@gmail.com>
2014-11-27 11:41:20 -0500

    Update Main.java

--- calculadora/calculadora/src/main/Main.java
+++ calculadora/calculadora/src/main/Main.java
@@ -13,14 +13,14 @@ public class Main {
 	int op=0;
         double n1,n2,s,m,d,r;
         do{
-            op=Integer.parseInt(JOptionPane.showInputDialog("nCalculadoran"+
-            "************n"+
-            "[1] SUMARn"+
-            "[2] RESTARn"+
-            "[3] MULTIPLICARn"+
-            "[4] DIVIDIRn"+
-            "[5] SALIRn"+
-            "Ingresa una opcion:"));
+            op=Integer.parseInt(JOptionPane.showInputDialog("\nCalculadora\n"+
+            "************************\n"+
+            "[1] SUMAR\n"+
+            "[2] RESTAR\n"+
+            "[3] MULTIPLICAR\n"+
+            "[4] DIVIDIR\n"+
+            "[5] SALIR\n"+
+            "Ingresa una opcion:\n\n"));
 
             switch(op)
             {
