e9ecb104ee98221a7be43fbbd63f1d26dd17f7b3
sebastian <sebastian@localhost>
2014-11-27 16:56:12 -0500

    calculadora

--- calculadora/calculadora/src/main/Main.java
+++ calculadora/calculadora/src/main/Main.java
@@ -34,8 +34,8 @@ public class Main {
                     s=n1+n2;
                     JOptionPane.showMessageDialog(null,"La suma es:"+s);
                     break;
-		
-		case 2:
+		//fffffffffffffffffff
+            	case 2:
                     n1=Double.parseDouble(JOptionPane.showInputDialog("Ingrese numero 1"));
                     n2=Double.parseDouble(JOptionPane.showInputDialog("Ingrese numero 2"));
                     r=n1-n2;
